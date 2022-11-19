652ad6246637548e43f5f8d0a6d4d39a7184e2ce
Vivek Kandathil <vivekandath@gmail.com>
2022-11-18 20:52:57 -0500

    Add files via upload

---
+++ client/Screens/Schedules.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+function Schedules() {
+    return (
+        <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
+            <Text>Schedules Screen</Text>
+        </View>
+    )
+}
+
+export default Schedules
