254bf783753106b6c8f334aedad4d625991315fc
richardrxn <richard.rassokhine@mail.mcgill.ca>
2022-11-19 19:00:05 -0500

    added more datagen func

--- server/scheduler123/src/main/java/org/scheduler/DataGenerator.java
+++ server/scheduler123/src/main/java/org/scheduler/DataGenerator.java
@@ -6,6 +6,10 @@ import java.util.Random;
 public class DataGenerator {
     public ArrayList<Trailer> data = new ArrayList<>();
     private final Random rn = new Random();
+    private String[] nameList = {"John", "Becky", "Johnny", "Sam", "Vivek", "Liam", "Richard", "Jane", "Alexa", "Alex",
+            "Ingrid", "Charles", "Nikki", "Chris", "Ines", "Sammy", "Tom", "Isaac", "Ghandi", "Ewan", "Obiwan Kenobi", "Cassian Andor",
+            "Ben Solo", "Han Solo", "Luke Skywalker", "R2D2", "Fork", "Jim", "Toby", "Al", "Joe", "Brandon", "Donald", "Justin",
+            "Chen", "Hacker", "Hunter", "Amber", "Toro", "Trevor", "Michael", "Tony", "Drew"};
 
     public DataGenerator(Integer ptestAmount) {
         generateTestData(ptestAmount);
@@ -13,7 +17,7 @@ public class DataGenerator {
 
     public void generateTestData(Integer pTestAmount) {
         for (int i = 0; i < pTestAmount; i++) {
-            Carrier Ctemp = new Carrier(Integer.toString(i), rn.nextInt(100));
+            Carrier Ctemp = new Carrier(nameList[rn.nextInt(nameList.length)], rn.nextInt(100));
             Ctemp.processWorkTime(rn.nextDouble()*11);
 
             Trailer Ttemp = new Flatbed(rn.nextInt(500) + 500, 1000, Ctemp, 0.004 * rn.nextDouble() + 0.001);
